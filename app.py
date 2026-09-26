02ff78fca59e375d9b5e63b0d4d8cbec5d9bcb84
riteshprajapati756500-cloud <riteshprajapati756500@gmail.com>
2026-09-26 10:53:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import requests
 import json
 import os
-
+from huggingface_hub import InferenceClient
 app = Flask(__name__)
 
 OLLAMA_URL = "http://localhost:11434/api/generate"
